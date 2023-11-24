001c8c8e9296ca2fa619a1a8b3243fd180440275
mrvelezo7 <dmast3rmind_8@hotmail.com>
2023-11-24 01:14:57 -0500

    prueba 9

--- Boletines/assets/scripts/main.js
+++ Boletines/assets/scripts/main.js
@@ -1,2 +1,24 @@
-let container = document.querySelector(".container");
-console.log(container);
+// let container = document.querySelector(".container");
+// console.log(container);
+
+//REcorrer una lista o arreglo
+/* let links = document.querySelectorAll("a");
+links.forEach(function(link){
+    console.log(link);
+}) */
+
+/* let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(celda){
+    celda.addEventListener('click', function(){
+        console.log(this);
+    })
+}) */
+
+let cerrar = document.querySelectorAll(".close");
+
+cerrar.forEach(function(x){
+    x.addEventListener('click', function(){
+        console.log(x);
+    })
+})

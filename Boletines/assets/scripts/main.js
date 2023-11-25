19fe84a7e60fa13c207d618e2bfacf98c99b0132
mrvelezo7 <dmast3rmind_8@hotmail.com>
2023-11-25 10:39:06 -0500

    mas prueba

--- Boletines/assets/scripts/main.js
+++ Boletines/assets/scripts/main.js
@@ -15,10 +15,35 @@ celdas.forEach(function(celda){
     })
 }) */
 
-let cerrar = document.querySelectorAll(".close");
+let links = document.querySelectorAll(".close");
 
-cerrar.forEach(function(x){
-    x.addEventListener('click', function(){
-        console.log(x);
-    })
+//Evento click
+links.forEach(function(link){
+    link.addEventListener('click', function(ev){
+        ev.preventDefault();    //se cambio el comportamiento del elemento cerrar 'a'
+
+        let animation = document.querySelector(".content");
+        
+        //quitarle las clases de animación
+        animation.classList.remove("animate__bounce");
+        animation.classList.remove("animate__animated");
+
+        //agregarle animacion
+        animation.classList.add("animate__bounceOutLeft");
+        animation.classList.add("animate__animated");
+        
+        //REgresa a la pagina principal
+        location.href ='/';
+        
+        return false;
+    });
+});
+
+//Agregar o quitar clases
+let iconos = document.querySelectorAll("i");
+
+iconos.forEach(function(icono){
+    icono.classList.remove("fa-star-o");   //classList accede a todas las propiedades del elemento html
+    icono.classList.add("fa-star"); //el método add añade clases y el remove las quita
 })
+
